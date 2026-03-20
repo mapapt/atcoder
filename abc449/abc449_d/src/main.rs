5a24f33f4f5e9cfcf373db587b58654d15be3f27
mapapt <mapapt@gmail.com>
2026-03-21 02:10:44 +0900

    created: abc449

---
+++ abc449/abc449_d/src/main.rs
@@ -0,0 +1,120 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[allow(dead_code)]
+trait Int: std::fmt::Debug + Copy + Default + Ord + Eq + ShrAssign + SubAssign +
+    Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + BitAnd<Output=Self> +
+    TryInto<usize> + TryFrom<usize>
+{ fn chk_mul(self, rhs: Self) -> Option<Self>; }
+impl Int for u8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for usize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for isize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_to_string(placeholder).unwrap();
+        Tokens(placeholder.split_whitespace())
+    }
+    fn new_line(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_line(placeholder).unwrap();
+        Tokens(placeholder.split_whitespace())
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut placeholder = String::new();
+    let mut tokens = Tokens::new(&mut placeholder);
+
+    let l: i64 = tokens.next();
+    let r: i64 = tokens.next();
+    let d: i64 = tokens.next();
+    let u: i64 = tokens.next();
+
+    let mut ans = if l <= 0 && 0 <= r && d <= 0 && 0 <= u {
+        1
+    }
+    else {
+        0
+    };
+
+    for x in 1..=1000_000/2 {
+        let xx = x * 2;
+        let l1 = l.max(-xx);
+        let r1 = r.min(xx);
+        let d1 = d.max(-xx);
+        let u1 = u.min(xx);
+        debug!(l1, r1, d1, u1);
+        let a1 = if l1 <= r1 && d1 <= u1 { (r1 - l1 + 1) * (u1 - d1 + 1) } else { 0 };
+        debug!(a1);
+        let l2 = l.max(-xx + 1);
+        let r2 = r.min(xx - 1);
+        let d2 = d.max(-xx + 1);
+        let u2 = u.min(xx - 1);
+        let a2 = if l2 <= r2 && d2 <= u2 { (r2 - l2 + 1) * (u2 - d2 + 1) } else { 0 };
+        debug!(a2);
+        let a = a1 - a2;
+        debug!(a);
+        ans += a;
+    }
+    println!("{ans}");
+}

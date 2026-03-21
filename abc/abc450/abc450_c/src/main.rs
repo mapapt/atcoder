4808a6b8b00e70d67b4eda5209325c1eb77c4f6a
mapapt <mapapt@gmail.com>
2026-03-21 22:46:47 +0900

    created: abc450

---
+++ abc/abc450/abc450_c/src/main.rs
@@ -0,0 +1,144 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[allow(dead_code)]
+trait Int: std::fmt::Debug + Copy + Default + Ord + Eq + ShrAssign + SubAssign +
+    Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + BitAnd<Output=Self> +
+    TryInto<usize> + TryFrom<usize>
+{ fn chk_mul(self, rhs: Self) -> Option<Self>; }
+impl Int for u8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for u128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for usize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for i128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+impl Int for isize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_to_string(placeholder).unwrap();
+        Tokens(placeholder.split_whitespace())
+    }
+    fn new_line(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_line(placeholder).unwrap();
+        Tokens(placeholder.split_whitespace())
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut placeholder = String::new();
+    let mut tokens = Tokens::new(&mut placeholder);
+
+    let h: usize = tokens.next();
+    let w: usize = tokens.next();
+
+    let mut m = Vec::new();
+    for _ in 0..h {
+        let b = tokens.next_bytes(); // Vec<u8>
+        m.push(b);
+    }
+    debug!(m);
+
+    let mut g = vec![vec![false; w]; h];
+    let mut ans = 0;
+
+    for i in 0..h {
+        for j in 0..w {
+            let mut found = false;
+            let mut bound = false;
+            let mut q = VecDeque::new();
+            q.push_back((i, j));
+            while let Some((ii, jj)) = q.pop_back() {
+                if m[ii][jj] == b'.' && !g[ii][jj] {
+                    g[ii][jj] = true;
+                    found = true;
+                    if ii > 0 {
+                        q.push_back((ii - 1, jj));
+                    }
+                    else {
+                        bound = true;
+                    }
+                    if jj > 0 {
+                        q.push_back((ii, jj - 1));
+                    }
+                    else {
+                        bound = true;
+                    }
+                    if ii < h - 1 {
+                        q.push_back((ii + 1, jj));
+                    }
+                    else {
+                        bound = true;
+                    }
+                    if jj < w - 1 {
+                        q.push_back((ii, jj + 1));
+                    }
+                    else {
+                        bound = true;
+                    }
+                }
+            }
+            if found && !bound {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{ans}");
+}

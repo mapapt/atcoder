1e27b64f6c01fc81aadcd056b92eed72464e0709
mapapt <mapapt@gmail.com>
2026-04-03 22:42:33 +0900

    updated: main

--- template/src/main.rs
+++ template/src/main.rs
@@ -7,24 +7,6 @@ use std::{
     rc::*, cell::*,
 };
 
-#[allow(dead_code)]
-trait Int: std::fmt::Debug + Copy + Default + Ord + Eq + ShrAssign + SubAssign +
-    Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + BitAnd<Output=Self> +
-    TryInto<usize> + TryFrom<usize>
-{ fn chk_mul(self, rhs: Self) -> Option<Self>; }
-impl Int for u8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for usize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for i8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for i16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for i32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for i64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for i128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for isize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-
 #[cfg(not(debug_assertions))]
 macro_rules! debug {
     ( $($x:tt)* ) => {};
@@ -49,33 +31,69 @@ macro_rules! debug {
     };
 }
 
-struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+struct StdIo<'a> {
+    tokens: std::str::SplitWhitespace<'a>,
+    delim: char,
+}
 
 #[allow(dead_code)]
-impl<'a> Tokens<'a> {
+impl<'a> StdIo<'a> {
     fn new(placeholder: &'a mut String) -> Self {
         placeholder.clear();
         std::io::stdin().read_to_string(placeholder).unwrap();
-        Tokens(placeholder.split_whitespace())
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
     }
     fn new_line(placeholder: &'a mut String) -> Self {
         placeholder.clear();
         std::io::stdin().read_line(placeholder).unwrap();
-        Tokens(placeholder.split_whitespace())
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
     }
     fn next_string(&mut self) -> String {
-        self.0.next().unwrap().to_string()
+        self.tokens.next().unwrap().to_string()
     }
     fn next_bytes(&mut self) -> Vec<u8> {
-        self.0.next().unwrap().as_bytes().to_vec()
+        self.tokens.next().unwrap().as_bytes().to_vec()
     }
     fn next<T>(&mut self) -> T
     where T: std::str::FromStr, T::Err: std::fmt::Debug {
-        self.0.next().unwrap().parse().unwrap()
+        self.tokens.next().unwrap().parse().unwrap()
     }
     fn collect<T, C>(&mut self, n: usize) -> C
     where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
         (0..n).map(|_| self.next()).collect()
+    }
+    fn put<T>(&mut self, val: T)
+    where T: std::fmt::Display {
+        if self.delim != '\0' {
+            print!("{}", self.delim);
+        }
+        print!("{}", val);
+        self.delim = ' ';
+    }
+    fn puti<A, T>(&mut self, val: A)
+    where A: AsRef<[T]>, T: std::fmt::Display {
+        for i in val.as_ref() {
+            self.put(i);
+        }
+    }
+    fn putn(&mut self) {
+        println!();
+        self.delim = '\0';
+    }
+    fn puty(&mut self, yes: bool) {
+        if yes {
+            self.put("Yes");
+        }
+        else {
+            self.put("No");
+        }
+        self.putn();
     }
 }
 
@@ -83,23 +101,35 @@ impl<'a> Tokens<'a> {
 
 fn main() {
     let mut placeholder = String::new();
-    let mut tokens = Tokens::new(&mut placeholder);
+    let mut io = StdIo::new(&mut placeholder);
 
 #[cfg(feature = "template")]
 {
-    let s = tokens.next_string(); // String
-    let b = tokens.next_bytes(); // Vec<u8>
-    let n: usize = tokens.next();
-    let a: Vec<u32> = tokens.collect(n);
+    let s = io.next_string(); // String
+    let b = io.next_bytes(); // Vec<u8>
+    let n: usize = io.next();
+    let a: Vec<u32> = io.collect(n);
+
+    debug!(s, b, n, a);
+    io.put(s);
+    io.puti(&b);
+    io.put(n);
+    io.puti(&a);
+    io.putn();
+
     let i: BTreeSet<(usize, u32)> = (0..n).map(|i|
-        (i, tokens.next())
+        (i, io.next())
     ).collect();
 
-    debug!(s, b, n, a, i);
+    debug!(i);
+    io.puty(i.contains(&(0, 2)));
 
     let b0 = char::from_u32(b[0] as u32).unwrap(); // u8 -> char
     let bs = String::from_utf8(b).unwrap(); // Vec<u8> -> String
 
     debug!(b0, bs);
+    io.put(b0);
+    io.put(&bs);
+    io.puty(bs.len() != 4);
 }
 }

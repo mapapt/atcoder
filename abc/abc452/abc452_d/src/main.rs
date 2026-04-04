95db561a25f98f3c69f41ad26867122040858ee2
mapapt <mapapt@gmail.com>
2026-04-05 01:27:37 +0900

    updated: abc452_d

---
+++ abc/abc452/abc452_d/src/main.rs
@@ -0,0 +1,139 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct StdIo<'a> {
+    tokens: std::str::SplitWhitespace<'a>,
+    delim: char,
+}
+
+#[allow(dead_code)]
+impl<'a> StdIo<'a> {
+    fn new(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_to_string(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn new_line(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_line(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn next_string(&mut self) -> String {
+        self.tokens.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.tokens.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.tokens.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn put<T>(&mut self, val: T)
+    where T: std::fmt::Display {
+        if self.delim != '\0' {
+            print!("{}", self.delim);
+        }
+        print!("{}", val);
+        self.delim = ' ';
+    }
+    fn puti<A, T>(&mut self, val: A)
+    where A: AsRef<[T]>, T: std::fmt::Display {
+        for i in val.as_ref() {
+            self.put(i);
+        }
+    }
+    fn putn(&mut self) {
+        println!();
+        self.delim = '\0';
+    }
+    fn puty(&mut self, yes: bool) {
+        if yes {
+            self.put("Yes");
+        }
+        else {
+            self.put("No");
+        }
+        self.putn();
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut placeholder = String::new();
+    let mut io = StdIo::new(&mut placeholder);
+
+    let s: Vec<char> = io.next_string().chars().collect();
+    let t: Vec<char> = io.next_string().chars().collect();
+    debug!(s, t);
+
+    let mut d = vec![vec![0; s.len() + 1]; t.len() + 1];
+
+    for is in 1..=s.len() {
+        for it in 0..t.len() {
+            if s[is - 1] == t[it] {
+                d[it + 1][is] += d[it][is - 1];
+            }
+            else {
+                d[it][is] += d[it][is - 1];
+            }
+        }
+
+        d[t.len()][is] += d[t.len()][is - 1];
+
+        if s[is - 1] == t[0] {
+            d[1][is] += 1;
+        }
+        else {
+            d[0][is] += 1;
+        }
+    }
+    debug!(d);
+
+    let x: usize = d[t.len()].iter().sum();
+    let y = (s.len() + 1) * s.len() / 2;
+    let z = y - x;
+    debug!(x, y, z);
+
+    io.put(z);
+}

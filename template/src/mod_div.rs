33670613c1d10690f1fd56638dcfb73ca9890e3d
mapapt <mapapt@gmail.com>
2026-05-23 23:39:30 +0900

    fix: mod_div

--- template/src/mod_div.rs
+++ template/src/mod_div.rs
@@ -1,23 +1,17 @@
 use std::ops::*;
 
+//#############################################################################
+
 trait Int: std::fmt::Debug + Copy + Default + Ord + Eq + ShrAssign + SubAssign +
     Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + BitAnd<Output=Self> +
     TryInto<usize> + TryFrom<usize>
 { fn chk_mul(self, rhs: Self) -> Option<Self>; }
-impl Int for u8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for u128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-impl Int for usize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for i8 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for i16 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for i32 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for i64 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for i128 { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
 impl Int for isize { fn chk_mul(self, rhs: Self) -> Option<Self> {self.checked_mul(rhs)} }
-
-//#############################################################################
 
 // (g, x, y) s.t. a x + b y = gcd(a, b) = g
 fn ext_euclid<N: Int>(a: N, b: N) -> (N, N, N)

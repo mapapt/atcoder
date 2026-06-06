abf312180bdd71cbe75670d793c93fea45661bfb
mapapt <mapapt@gmail.com>
2026-06-06 22:42:13 +0900

    created: abc461

---
+++ abc/abc461/abc461_c/src/main.rs
@@ -0,0 +1,144 @@
+use std::prelude::rust_2024::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct StdIo<'a> {
+    tokens: std::str::SplitWhitespace<'a>,
+    delim: char,
+}
+
+#[allow(dead_code)]
+impl<'a> StdIo<'a> {
+    fn new(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_to_string(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn new_line(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_line(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn next_string(&mut self) -> String {
+        self.tokens.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.tokens.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.tokens.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn put<T>(&mut self, val: T)
+    where T: std::fmt::Display {
+        if self.delim != '\0' {
+            print!("{}", self.delim);
+        }
+        print!("{}", val);
+        self.delim = ' ';
+    }
+    fn puti<A, T>(&mut self, val: A)
+    where A: AsRef<[T]>, T: std::fmt::Display {
+        for i in val.as_ref() {
+            self.put(i);
+        }
+    }
+    fn putn(&mut self) {
+        println!();
+        self.delim = '\0';
+    }
+    fn puty(&mut self, yes: bool) {
+        if yes {
+            self.put("Yes");
+        }
+        else {
+            self.put("No");
+        }
+        self.putn();
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut placeholder = String::new();
+    let mut io = StdIo::new(&mut placeholder);
+
+    let n: usize = io.next();
+    let k: usize = io.next();
+    let m: usize = io.next();
+
+    let mut g = HashMap::new();
+    for _ in 0..n {
+        let c: usize = io.next();
+        let v: u64 = io.next();
+        if !g.contains_key(&c) {
+            g.insert(c, Vec::new());
+        }
+        g.get_mut(&c).unwrap().push(v);
+    }
+    debug!(g);
+
+    let mut vc = Vec::new();
+    let mut vr = Vec::new();
+    for (_, a) in g.iter_mut() {
+        a.sort();
+        vc.push(a.pop().unwrap());
+        vr.append(a);
+    }
+
+    let mut ans = 0;
+
+    vc.sort();
+    for _ in 0..m {
+        ans += vc.pop().unwrap();
+    }
+    vr.append(&mut vc);
+
+    vr.sort();
+    for _ in m..k {
+        ans += vr.pop().unwrap();
+    }
+
+    io.put(ans);
+    io.putn();
+}

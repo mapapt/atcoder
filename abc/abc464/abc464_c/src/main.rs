83dd4fa20f68e7ff6ad0deb97dc9aa52e86d2af0
mapapt <mapapt@gmail.com>
2026-06-27 22:41:31 +0900

    created: abc464

---
+++ abc/abc464/abc464_c/src/main.rs
@@ -0,0 +1,136 @@
+use std::prelude::rust_2024::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct StdIo<'a> {
+    tokens: std::str::SplitWhitespace<'a>,
+    delim: char,
+}
+
+#[allow(dead_code)]
+impl<'a> StdIo<'a> {
+    fn new(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_to_string(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn new_line(placeholder: &'a mut String) -> Self {
+        placeholder.clear();
+        std::io::stdin().read_line(placeholder).unwrap();
+        StdIo {
+            tokens: placeholder.split_whitespace(),
+            delim: '\0',
+        }
+    }
+    fn next_string(&mut self) -> String {
+        self.tokens.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.tokens.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.tokens.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn put<T>(&mut self, val: T)
+    where T: std::fmt::Display {
+        if self.delim != '\0' {
+            print!("{}", self.delim);
+        }
+        print!("{}", val);
+        self.delim = ' ';
+    }
+    fn puti<A, T>(&mut self, val: A)
+    where A: AsRef<[T]>, T: std::fmt::Display {
+        for i in val.as_ref() {
+            self.put(i);
+        }
+    }
+    fn putn(&mut self) {
+        println!();
+        self.delim = '\0';
+    }
+    fn puty(&mut self, yes: bool) {
+        if yes {
+            self.put("Yes");
+        }
+        else {
+            self.put("No");
+        }
+        self.putn();
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut placeholder = String::new();
+    let mut io = StdIo::new(&mut placeholder);
+
+    let n: usize = io.next();
+    let m: usize = io.next();
+    let mut adb: Vec<(u32, usize, u32)> = (0..n).map(|_| (io.next(), io.next(), io.next())).collect();
+    debug!(adb);
+    adb.sort_by(|a, b| a.1.cmp(&b.1));
+    debug!(adb);
+    let mut c = BTreeMap::new();
+    for x in adb.iter() {
+        let a = x.0;
+        c.entry(a).and_modify(|e| *e += 1).or_insert(1);
+    }
+    debug!(c);
+    let mut day = 1;
+    for x in adb.iter() {
+        let (a, d, b) = *x;
+        while day < d {
+            io.put(c.len());
+            io.putn();
+            day += 1;
+        }
+        c.entry(a).and_modify(|e| *e -= 1).or_insert(0);
+        if c[&a] == 0 {
+            c.remove(&a);
+        }
+        c.entry(b).and_modify(|e| *e += 1).or_insert(1);
+    }
+    for _ in day..=m {
+        io.put(c.len());
+        io.putn();
+    }
+}
